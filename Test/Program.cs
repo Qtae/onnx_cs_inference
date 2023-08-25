c06d4d87a721e83745a89c00e9669cc9e5b60884
Qtae <qtkim91@gmail.com>
2023-08-25 17:33:54 +0900

    csharp onnx

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATI_ONNX;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ONNXCore onnxCore = new ONNXCore();
+            onnxCore.LoadModel("./model.onnx", true, true, "");
+        }
+    }
+}
