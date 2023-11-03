cfa57c307b18833d0bd5fe505a23617aa89a2ad3
Qtae <qtkim91@gmail.com>
2023-11-03 14:19:42 +0900

    bump3d get height function

--- ONNX_Inference/Bump3D.cs
+++ ONNX_Inference/Bump3D.cs
@@ -3,7 +3,7 @@ using Microsoft.ML.OnnxRuntime.Tensors;
 using Microsoft.ML.OnnxRuntime;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
+using System.Linq;
 
 namespace ONNX_Inference
 {
@@ -14,49 +14,107 @@ namespace ONNX_Inference
         public Bump3D(string modelPath, bool bTensorRT, bool bUseCache, string cachePath = "", ulong maxWorkspaceSize = 1ul << 60)
             : base(modelPath, bTensorRT, bUseCache, cachePath, maxWorkspaceSize) { }
 
-        public byte[] GetHeightMap(byte[,,,] input, int batch)
+        public float[,,] GetHeightMap(float[,,,] input, int batch)
         {
             try
             {
                 int nImages = input.GetLength(0);
                 int nRow = input.GetLength(1);
-                int mFOV = input.GetLength(2);
+                int nFOV = input.GetLength(2);
                 int nFrames = input.GetLength(3);
+                int outputLengthPerImage = nFOV * nFrames;
 
-                byte[] heightMap = new byte[nImages * mFOV * nFrames];
+                float[,,] heightMap = new float[nImages,nFOV,nFrames];
 
                 List<List<int>> inputDims = GetInputDims();
-                if (inputDims[0][1] != nRow || inputDims[0][2] != mFOV || inputDims[0][3] != nFrames)
+                if (inputDims[0][1] != nRow || inputDims[0][2] != nFOV || inputDims[0][3] != nFrames)
                     throw new Exception("Input dimension is invalid.");
 
                 int[] tensorShape = inputDims[0].ToArray(); //assume that their is only one input operator.
+                tensorShape[0] = 1;
+                int lengthPerImage = 1;
+                foreach (int var in tensorShape) lengthPerImage *= var;
                 tensorShape[0] = batch;
-                DenseTensor<float> inputTensor = new DenseTensor<float>(tensorShape);
+                int lengthPerBatch = lengthPerImage * batch;
+                int outputLengthPerBatch = outputLengthPerImage * batch;
 
-                for (int batchIdx = 0; batchIdx < nImages / batch + (nImages % batch == 0 ? 0 : 1) ; ++batchIdx)
+                // Parallel.For(0, nImages / batch, batchIdx =>
+                // {
+                //     float[] batchInput = new float[lengthPerBatch];
+                //     Buffer.BlockCopy(input, batchIdx * lengthPerBatch * 4, batchInput, 0, lengthPerBatch * 4);
+                //     Memory<float> inputMem = new Memory<float>(batchInput);
+                //     DenseTensor<float> inputTensor = new DenseTensor<float>(inputMem, tensorShape);
+                // 
+                //     int batchStart = batchIdx * batch;
+                //     int batchEnd = (batchIdx + 1) * batch;
+                // 
+                //     NamedOnnxValue inputNamedOnnxValue
+                //         = NamedOnnxValue.CreateFromTensor(GetInputNames()[0], inputTensor);
+                //     List<NamedOnnxValue> inputs = new List<NamedOnnxValue>{ inputNamedOnnxValue };
+                //     IReadOnlyCollection<string> outputNames = new List<string> { GetOutputNames()[0] };
+                //     IDisposableReadOnlyCollection<DisposableNamedOnnxValue> res = Run(inputs, outputNames);
+                // 
+                //     Buffer.BlockCopy(
+                //         res.ToArray()[0].AsTensor<float>().ToArray(),
+                //         0,
+                //         heightMap,
+                //         batchIdx * outputLengthPerBatch * 4,
+                //         outputLengthPerBatch * 4
+                //         );
+                // });
+
+                for (int batchIdx = 0; batchIdx < nImages / batch; ++batchIdx)
                 {
+                    float[] batchInput = new float[lengthPerBatch];
+                    Buffer.BlockCopy(input, batchIdx * lengthPerBatch * 4, batchInput, 0, lengthPerBatch * 4);
+                    Memory<float> inputMem = new Memory<float>(batchInput);
+                    DenseTensor<float> inputTensor = new DenseTensor<float>(inputMem, tensorShape);
+
                     int batchStart = batchIdx * batch;
                     int batchEnd = (batchIdx + 1) * batch;
-                    Parallel.For(batchStart, batchEnd, imgIdx =>
-                    {
-                        for (int rowIdx = 0; rowIdx < nRow; ++rowIdx)
-                        {
-                            for (int fovIdx = 0; fovIdx < mFOV; ++fovIdx)
-                            {
-                                for (int frameIdx = 0; frameIdx < nFrames; ++frameIdx)
-                                {
-                                    inputTensor[imgIdx - batchStart, rowIdx, fovIdx, frameIdx, 0] =
-                                        input[imgIdx, rowIdx, fovIdx, frameIdx];
-                                }
-                            }
-                        }
-                    });
+                
+                    NamedOnnxValue inputNamedOnnxValue
+                        = NamedOnnxValue.CreateFromTensor(GetInputNames()[0], inputTensor);
+                    List<NamedOnnxValue> inputs = new List<NamedOnnxValue> { inputNamedOnnxValue };
+                    IReadOnlyCollection<string> outputNames = new List<string> { GetOutputNames()[0] };
+                    IDisposableReadOnlyCollection<DisposableNamedOnnxValue> res = Run(inputs, outputNames);
+
+                    Buffer.BlockCopy(
+                        res.ToArray()[0].AsTensor<float>().ToArray(),
+                        0,
+                        heightMap,
+                        batchIdx * outputLengthPerBatch * 4,
+                        outputLengthPerBatch * 4
+                        );
+                }
+
+                int residue = nImages % batch;
+                tensorShape[0] = residue;
+                int lengthOfResidue = lengthPerImage * residue;
+                int outputLengthOfResidue = outputLengthPerImage * residue;
+
+                if (residue != 0)
+                {
+                    int batchStart = nImages - residue;
+
+                    float[] batchInput = new float[lengthOfResidue];
+                    Buffer.BlockCopy(input, batchStart * lengthPerImage * 4, batchInput, 0, lengthOfResidue * 4);
+                    Memory<float> inputMem = new Memory<float>(batchInput);
+                    DenseTensor<float> inputTensor = new DenseTensor<float>(inputMem, tensorShape);
 
                     NamedOnnxValue inputNamedOnnxValue
                         = NamedOnnxValue.CreateFromTensor(GetInputNames()[0], inputTensor);
-                    List<NamedOnnxValue> inputs = new List<NamedOnnxValue>{ inputNamedOnnxValue };
+                    List<NamedOnnxValue> inputs = new List<NamedOnnxValue> { inputNamedOnnxValue };
                     IReadOnlyCollection<string> outputNames = new List<string> { GetOutputNames()[0] };
                     IDisposableReadOnlyCollection<DisposableNamedOnnxValue> res = Run(inputs, outputNames);
+
+                    Buffer.BlockCopy(
+                        res.ToArray()[0].AsTensor<float>().ToArray(),
+                        0,
+                        heightMap,
+                        batchStart * outputLengthPerImage * 4,
+                        outputLengthOfResidue * 4
+                        );
                 }
 
                 return heightMap;
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Diagnostics;
+using System.Drawing;
+using System.Threading.Tasks;
 using ONNX_Inference;
-//using SixLabors.ImageSharp;
 
 namespace Test
 {
@@ -17,31 +18,68 @@ namespace Test
         }
         static void Main(string[] args)
         {
-            string modelPath = "D:\\QTAE\\CS_ONNX_Inference\\models\\3d_bump.onnx";
-            string cachePath = "D:\\QTAE\\CS_ONNX_Inference\\models\\";
-            Bump3D bump3dAI = new Bump3D(modelPath, true, true, cachePath);
-            byte[,,,] tmp = new byte[100000, 56, 12, 12];
-            Random random = new Random();
-            for (int i = 0; i < tmp.GetLength(0); i++)
+            Console.WriteLine("==========Initializing...==========");
+            float[,,,] tmp = new float[1000, 56, 12, 12];
+
+            string rootPath = "D:\\QTAE\\CS_ONNX_Inference\\";
+            string testDataPath = rootPath + "testset\\";
+
+            Console.WriteLine("==========Load Images...==========");
+            System.IO.DirectoryInfo testRootDirInfo = new System.IO.DirectoryInfo(testDataPath);
+            int setIdx = 0;
+            foreach (System.IO.DirectoryInfo subDir in testRootDirInfo.GetDirectories())
             {
-                for (int j = 0; j < tmp.GetLength(1); j++)
+                string testDataSubDirPath = subDir.FullName;
+                string convertedDataPath = testDataSubDirPath + "\\converted";
+                System.IO.DirectoryInfo convertedDirInfo = new System.IO.DirectoryInfo(convertedDataPath);
+                int imgIdx = 0;
+                foreach (System.IO.FileInfo imgFile in convertedDirInfo.GetFiles())
                 {
-                    for (int k = 0; k < tmp.GetLength(2); k++)
+                    if (imgFile.Extension.ToLower().CompareTo(".png") == 0)
                     {
-                        for (int n = 0; n < tmp.GetLength(3); n++)
-                            tmp[i, j, k, n] = (byte)random.Next(256);
+                        Bitmap img = new Bitmap(imgFile.FullName);
+                        for(int i = 0; i < 12; ++i)
+                        {
+                            for (int j = 0; j < 56; ++j)
+                            {
+                                tmp[setIdx, j, i, imgIdx] = img.GetPixel(i, j).R;
+                            }
+                        }
                     }
+                    imgIdx++;
                 }
+                setIdx++;
+                if (setIdx == 1000) break;
             }
+
+            Console.WriteLine("==========Load Model...==========");
+            string cachePath = rootPath + "models\\";
+            string modelPath = cachePath + "3d_bump.onnx";
+            Bump3D bump3dAI = new Bump3D(modelPath, true, true, cachePath);
+            int batch = 32;
+            Console.WriteLine("==========Run Inference...==========");
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            byte[] res = bump3dAI.GetHeightMap(tmp, 16);
+            float[,,] res = bump3dAI.GetHeightMap(tmp, batch);
             sw.Stop();
             Console.WriteLine("소요 시간: {0}ms", sw.ElapsedMilliseconds);
-            //bool res = onnx.LoadModel(modelPath, true, true, cachePath);
-            System.Console.WriteLine("Success!");
-            //IEnumerator test = Test();
-            //test.MoveNext();
+            // sw.Restart();
+            // res = bump3dAI.GetHeightMap(tmp, batch);
+            // sw.Stop();
+            // Console.WriteLine("소요 시간: {0}ms", sw.ElapsedMilliseconds);
+            // sw.Restart();
+            // res = bump3dAI.GetHeightMap(tmp, batch);
+            // sw.Stop();
+            // Console.WriteLine("소요 시간: {0}ms", sw.ElapsedMilliseconds);
+            // sw.Restart();
+            // res = bump3dAI.GetHeightMap(tmp, batch);
+            // sw.Stop();
+            // Console.WriteLine("소요 시간: {0}ms", sw.ElapsedMilliseconds);
+            // sw.Restart();
+            // res = bump3dAI.GetHeightMap(tmp, batch);
+            // sw.Stop();
+            // Console.WriteLine("소요 시간: {0}ms", sw.ElapsedMilliseconds);
+            System.Console.WriteLine("==========Success!==========");
             System.Console.ReadKey();
         }
     }
