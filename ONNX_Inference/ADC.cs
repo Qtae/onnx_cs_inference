8bd8935af2a11ea6f1576c81f47f4564905887ea
Qtae <qtkim91@gmail.com>
2023-11-03 16:45:28 +0900

    add adc features

--- ONNX_Inference/ADC.cs
+++ ONNX_Inference/ADC.cs
@@ -1,4 +1,8 @@
 ﻿
+using System.Collections.Generic;
+using System;
+using Microsoft.ML.OnnxRuntime;
+
 namespace ONNX_Inference
 {
     public class ADC : ONNXCore
@@ -7,5 +11,40 @@ namespace ONNX_Inference
 
         public ADC(string modelPath, bool bTensorRT, bool bUseCache, string cachePath = "", ulong maxWorkspaceSize = 1ul << 60)
             : base(modelPath, bTensorRT, bUseCache, cachePath, maxWorkspaceSize) { }
+
+        public float[,] RunADCandGetSoftmax(float[,,,] input, int batch)
+        {
+            try
+            {
+                int nImages = input.GetLength(0);
+                int nHeight = input.GetLength(1);
+                int nWidth = input.GetLength(2);
+                int nChannel = input.GetLength(3);
+
+                List<List<int>> inputDims = GetInputDims();
+                if (inputDims[0][1] != nHeight || inputDims[0][2] != nWidth || inputDims[0][3] != nChannel)
+                    throw new Exception("Input dimension is invalid.");
+
+                List<List<int>> outputDims = GetOutputDims();
+                int nClass = outputDims[0][1];
+
+                float[,] res = new float[nImages, nClass];
+
+                return res;
+            }
+            catch (OnnxRuntimeException ex)
+            {
+                System.Console.WriteLine("Error in RunADCandGetSoftmax() :");
+                System.Console.WriteLine(ex.Message);
+                throw;
+            }
+
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("Error in RunADCandGetSoftmax() :");
+                System.Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -10,13 +10,7 @@ namespace Test
 {
     class Program
     {
-        static IEnumerator Test()
-        {
-            Console.WriteLine("1");
-            Console.WriteLine("2");
-            Console.WriteLine("3");
-            yield return null;
-        }
+        /*
         static void Main(string[] args)
         {
             Console.WriteLine("==========Initializing...==========");
@@ -99,6 +93,70 @@ namespace Test
             }
 
 
+
+            System.Console.WriteLine("==========Success!==========");
+            System.Console.ReadKey();
+        }
+        */
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Initializing...==========");
+            float[,,,] tmp = new float[30, 32, 32, 3];
+
+            string rootPath = "D:\\QTAE\\CS_ONNX_Inference\\";
+            string testDataPath = rootPath + "testset_classification\\";
+
+            Console.WriteLine("==========Load Images...==========");
+            System.IO.DirectoryInfo testDirInfo = new System.IO.DirectoryInfo(testDataPath);
+            int imgIdx = 0;
+            foreach (System.IO.FileInfo imgFile in testDirInfo.GetFiles())
+            {
+                if (imgFile.Extension.ToLower().CompareTo(".png") == 0)
+                {
+                    Bitmap img = new Bitmap(imgFile.FullName);
+                    for (int i = 0; i < 32; ++i)
+                    {
+                        for (int j = 0; j < 32; ++j)
+                        {
+                            tmp[imgIdx, j, i, 0] = img.GetPixel(i, j).R;
+                            tmp[imgIdx, j, i, 1] = img.GetPixel(i, j).G;
+                            tmp[imgIdx, j, i, 2] = img.GetPixel(i, j).B;
+                        }
+                    }
+                }
+                imgIdx++;
+            }
+
+            Console.WriteLine("==========Load Model...==========");
+            string cachePath = rootPath + "models\\";
+            string modelPath = cachePath + "classification.onnx";
+            ADC bump3dAI = new ADC(modelPath, true, true, cachePath);
+            int batch = 4;
+            Console.WriteLine("==========Run Inference...==========");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            float[,] res = bump3dAI.RunADCandGetSoftmax(tmp, batch);
+            sw.Stop();
+            Console.WriteLine("소요 시간: {0}ms", sw.ElapsedMilliseconds);
+
+            Console.WriteLine("==========Save result...==========");
+            for (int i = 0; i < 1000; ++i)
+            {
+                Bitmap result = new Bitmap(12, 12);
+                for (int x = 0; x < 12; ++x)
+                {
+                    for (int y = 0; y < 12; ++y)
+                    {
+                        float pixelFloat = res[i, x, y] * 255.0f / 56.0f;
+                        int pixel = (int)pixelFloat;
+                        Color col = Color.FromArgb(pixel, pixel, pixel);
+                        result.SetPixel(y, x, col);
+                    }
+                }
+                result.Save(rootPath + "test_res\\" + i.ToString() + ".bmp");
+            }
 
             System.Console.WriteLine("==========Success!==========");
             System.Console.ReadKey();
